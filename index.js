f6553d426030ea616313dab84ef7bf85d5a5cd8a
Dyumna Karthikeyan <dyumna.karthikeyan2023@vitstudent.ac.in>
2025-02-15 00:15:21 +0530

    sidebar added by neeraj

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ signUp.addEventListener("click", ()=>
     {
         open('signup.html', "_blank");
     });
-
+function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginRigh = "250px";
+      }
+      
+      /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+      }
