c36cf02ddfcbaa7a55e55cc8ab48735c709bff1a
Dyumna Karthikeyan <dyumna.karthikeyan2023@vitstudent.ac.in>
2025-02-14 21:09:50 +0530

    for neeraj's ref

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO - To link each of the buttons to their seperate pages
 
-const login = document.getElementById('login');
-const signUp = document.getElementById('signUp');
+const login = document.getElementById('button1');
+const signUp = document.getElementById('button2');
 login.addEventListener("click", ()=>
 {
     open('login.html', "_blank");
