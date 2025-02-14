815cd45ac51c0f4f1601fdecc46e55e40a343aba
Neeraj Prasanna <neeraj.learning123@gmail.com>
2025-02-14 16:04:05 +0530

    index.js updated to open login page from application page

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // TODO - To link each of the buttons to their seperate pages
+
+const login = document.getElementById('login');
+const signUp = document.getElementById('signUp');
+login.addEventListener("click", ()=>
+{
+    open('login.html', "_blank");
+})
